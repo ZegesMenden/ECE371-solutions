217c73287511f69f8e540012c941b25bb461e459
roberto7176 <rqdidonna@gmail.com>
2025-10-14 20:05:43 -0400

    buzzer.py

--- Lab2-1/Lab2/buzzer.py
+++ Lab2-1/Lab2/buzzer.py
@@ -7,11 +7,24 @@ from time import sleep
 def success():
     """3 short beeps."""
     # TODO: on/off pattern
+    buzz = bz(2)
+    #sets one longn buzzer that is 0.5 seconds long 
+    try:
+        bz.beep(on_time = .1, off_time = .1, n = 3, background = False)
+    finally:
+        bz.close()
     pass
 
 def fail():
     """1 long beep."""
     # TODO: on/off pattern
+     #bz.on, sleep(10, bz.off 
+    buzz = bz(2)
+    #sets one longn buzzer that is 0.5 seconds long 
+    try:
+        bz.beep(on_time = 1, off_time = 1, n = 1, background = False)
+    finally:
+        bz.close()
     pass
 
 def cleanup():
