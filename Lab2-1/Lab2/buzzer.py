e395c8709e0a6dbaad941137352cd9f027d166c2
ZegesMenden <camkullberg@gmail.com>
2025-10-20 14:55:20 -0400

    Update buzzer.py

--- Lab2-1/Lab2/buzzer.py
+++ Lab2-1/Lab2/buzzer.py
@@ -1,13 +1,12 @@
-# from gpiozero import Buzzer
+from gpiozero import Buzzer
 from time import sleep
 
-# bz =  None   # replace None with the correct pin number
-# bz.off() 
+bz = Buzzer(2)
+bz.off() 
 
 def success():
     """3 short beeps."""
     # TODO: on/off pattern
-    buzz = bz(2)
     #sets one longn buzzer that is 0.5 seconds long 
     try:
         bz.beep(on_time = .1, off_time = .1, n = 3, background = False)
@@ -18,8 +17,7 @@ def success():
 def fail():
     """1 long beep."""
     # TODO: on/off pattern
-     #bz.on, sleep(10, bz.off 
-    buzz = bz(2)
+    #bz.on, sleep(10, bz.off 
     #sets one longn buzzer that is 0.5 seconds long 
     try:
         bz.beep(on_time = 1, off_time = 1, n = 1, background = False)
@@ -28,6 +26,5 @@ def fail():
     pass
 
 def cleanup():
-    pass
-    # bz.off()
-    # bz.close()
+    bz.off()
+    bz.close()
